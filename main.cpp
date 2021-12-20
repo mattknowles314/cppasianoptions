8cefee330a11fbebadf945441e5a0b9f959bebe4
Matthew Knowles <mattknowles314@gmail.com>
2021-12-20 21:41:52 +0000

    Integrating the code from test into the main project

--- main.cpp
+++ main.cpp
@@ -10,11 +10,18 @@ int main(){
     double U = 0.5;
     double D = 0.3;
     int N = 5;
-    int testPath[5] = {0,0,0,1,0};
+    int testPath[5];
     int *p = testPath;
+    int x = 3;
+    GenPathByNumber(x,5,p);
     double testPrice[6] = {0};
     double *q = testPrice;
     GenPricesByPath(S0,U,D,p,N,q);
+    cout << "PATH" << endl;
+    for(int j=0;j<N;j++){
+        cout<<testPath[j]<<endl;
+    }
+    cout << "PRICES" << endl;
     for(int j=0;j<N;j++){
         cout<<testPrice[j]<<endl;
     }
