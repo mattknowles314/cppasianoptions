c33915137b4186bb161548b2f77d4525d14a4d9a
Matthew Knowles <mattknowles314@gmail.com>
2022-02-14 23:39:39 +0000

    Fixed issue #4

--- AsianOptions.h
+++ AsianOptions.h
@@ -29,6 +29,16 @@ double Price(double S0, double U, double D, double R, int N, double K, double (*
     int paths[rows][cols];
     int (*r)[cols] = paths;
 
+    //Create an empty 2^N x N matrix to store prices along the paths
+    double prices[rows][cols];
+    double (*s)[cols] = prices;
+
+    //Create empty array of payoffs
+    double payoffs[rows];
+	double *c = payoffs;
+
+
+
     //Generate paths by index
     for(int x=0; x<=rows;x++){
         int tempL[cols];
@@ -57,10 +67,6 @@ double Price(double S0, double U, double D, double R, int N, double K, double (*
 	
 	cout << "BREAK" << endl;
 	
-    //Create an empty 2^N x N matrix to store prices along the paths
-    double prices[rows][cols];
-    double (*s)[cols] = prices;
-    
     for(int x=0; x<=rows; x++){
         double tempPr[cols];
         double *u = tempPr;
@@ -69,11 +75,6 @@ double Price(double S0, double U, double D, double R, int N, double K, double (*
 			s[x][i] = u[i];
 		}
     }
-
-
-    //Create empty array of payoffs
-    double payoffs[rows];
-	double *c = payoffs;
 
     //Loop through each path x and calculate payoff along that path
     for(int x=0; x<=rows; x++){
